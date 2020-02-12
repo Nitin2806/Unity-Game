a220c37d03a4423b43ae177898966d83240a63b4
Nitin Mishra <mnitin2311@gmail.com>
2020-02-12 19:52:02 +0530

    synced project

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+public class playercontroller : MonoBehaviour
+{
+   public Rigidbody rb;
+   public float forwardforce=200f;
+   public float sideforce=500f;
+   void FixedUpdate()
+   {
+     
+     if(Input.GetKey("w"))
+     {
+       rb.AddForce(0,0,500 * Time.deltaTime);
+     }
+     if(Input.GetKey("d"))
+     {
+       rb.AddForce(500 * Time.deltaTime,0,0);
+     }
+     if(Input.GetKey("a"))
+     {
+       rb.AddForce(-sideforce* Time.deltaTime,0,0);
+     }
+     if(Input.GetKey("s"))
+     {
+       rb.AddForce(0,0,-500* Time.deltaTime);
+     }
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+     rb.AddForce (0,200,100);
+     }
+     rb.AddForce(0,-10,0);
+     }
+   }
+
+
+
